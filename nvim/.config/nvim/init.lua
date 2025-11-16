6e33f43c7b65612517c53113d124b2a834fc65b8
Michal <michal.lester@fourthwall.com>
2025-11-16 23:12:03 +0100

    add git worktree plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -345,7 +345,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>h', group = 'Harpoon', mode = { 'n', 'v' } },
       },
     },
   },
