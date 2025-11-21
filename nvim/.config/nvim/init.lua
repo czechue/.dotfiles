4173d765229eb46ddfeee22dc62951edd2810209
Michal Lester <michal.lester87@gmail.com>
2025-11-21 16:47:01 +0100

    feat: add ESLint auto-fix on save
    Added on_attach handler to ESLint LSP configuration that automatically
    runs 'source.fixAll.eslint' code action on BufWritePre.
    
    This fixes auto-fixable ESLint errors (like import spacing, unused vars
    with quick fixes, etc.) automatically when saving the file.
    
    Usage: Just save the file (:w) and ESLint will fix what it can.
    
    Tested in storefront-apps monorepo.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -787,6 +787,18 @@ require('lazy').setup({
               useFlatConfig = false,
             },
           },
+          -- Auto-fix ESLint errors on save
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.code_action({
+                  context = { only = { 'source.fixAll.eslint' } },
+                  apply = true,
+                })
+              end,
+            })
+          end,
         },
       }
 
